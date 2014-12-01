2408bb9acfd6488277c57fe010f87bcb2ce961d2
Supernut <joshloyens@gmail.com>
2014-12-01 16:24:38 -0700

    Happiness

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a project for working on Regular Expression
+This is a project for working on Regular Expression HAHAHAHA
--- src/io/github/supernut/RegExp/RegExp.java
+++ src/io/github/supernut/RegExp/RegExp.java
@@ -1,5 +1,11 @@
 package io.github.supernut.RegExp;
 
-public class RegExp {
+import java.util.regex.*;
 
+public class RegExp {
+	public static void main(String[] args){
+		Pattern pattern = Pattern.compile("");
+		Matcher matcher = pattern.matcher("This is the input string");
+		matcher.matches();
+	}
 }
